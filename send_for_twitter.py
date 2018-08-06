4b12311bc7e9f07282a785ba8bc25cedb78bba7a
Lucas Carrafa <lucascarrafa@hotmail.com>
2018-08-06 20:01:17 -0300

    Inserção dos comentários

--- send_for_twitter.py
+++ send_for_twitter.py
@@ -2,16 +2,18 @@ import twitter
 import time
 import serial
 
-arduino = serial.Serial('/dev/ttyUSB0',9600)
+arduino = serial.Serial('/dev/ttyUSB0',9600) #recebe os dados do arduino
 
+#inserir os tokens gerados pelo Twitter em https://apps.twitter.com/
 api = twitter.Api(consumer_key='',
                       consumer_secret='',
                       access_token_key='',
                       access_token_secret='')
 
+#funcao que posta os dados no usuario logado
 def resposta(temp, umid):
 	status = api.PostUpdate('São Mateus está com a temperatura de '+temp+' ºC e umidade de '+umid+'%')
-	print(status.text)
+	print(status.text) #imprime no terminal o conteudo que foi postado
 
 
 
@@ -21,6 +23,6 @@ while True:
 	
 	valor = arduino.readline().decode("utf-8")
 	umidade, temperatura = valor.split(' ')
-	resposta(temperatura[:5],umidade)
+	resposta(temperatura[:5],umidade) #os dados recebidos pelo arduino contem um \n no fim de cada linha, para eliminar, foi usado [:5] que pega apenas os cinco primeiros caracteres
 	#print(temperatura[:5])
-	time.sleep(60)  
+	time.sleep(60)  #espera 60 segundos para enviar a proxima postagem
